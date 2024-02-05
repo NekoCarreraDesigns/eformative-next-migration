d5f580de44aa64b4868f23b409f77f181acd6ac7
NekoCarreraDesigns <denvermaas@gmail.com>
2024-02-05 11:24:56 -0700

    added images to image folder, put links for images and manifests for migration

--- eformative-next-migration/src/pages/index.js
+++ eformative-next-migration/src/pages/index.js
@@ -13,6 +13,11 @@ export default function Home() {
         <meta name="description" content="eformative website for buying and selling of E-bikes, E-scooters, One Wheels, and other alternative transportation method" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
