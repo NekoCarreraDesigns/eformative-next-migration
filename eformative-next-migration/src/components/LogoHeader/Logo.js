b63728847a6aabd1567c4638880dbde6f9b586f3
NekoCarreraDesigns <denvermaas@gmail.com>
2024-02-05 12:36:47 -0700

    added logo.js for migration

---
+++ eformative-next-migration/src/components/LogoHeader/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <h1 className='page-header-logo'>
+      <img alt='logo' className='logo' src='/assets/eLogo.jpg'></img>
+      formative
+    </h1>
+  );
+};
+
+export default Logo;
