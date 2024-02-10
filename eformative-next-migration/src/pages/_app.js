c91e2b51e1d3e385cb31843c8aa2e29dd5e36268
NekoCarreraDesigns <denvermaas@gmail.com>
2024-02-10 16:10:03 -0700

    added footer to _app.ja

--- eformative-next-migration/src/pages/_app.js
+++ eformative-next-migration/src/pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer"
 
  function App() {
   return (
     <>
     <Navbar/>
+    <Footer/>
     </>
   )
 }
