bc832dbeedb43825b541336909ab50eae503596d
NekoCarreraDesigns <denvermaas@gmail.com>
2024-02-13 13:34:25 -0700

    added Landing page to _app.js

--- eformative-next-migration/src/pages/_app.js
+++ eformative-next-migration/src/pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer"
+import Landing from "./Landing";
 
  function App() {
   return (
     <>
     <Navbar/>
+    <Landing/>
     <Footer/>
     </>
   )
