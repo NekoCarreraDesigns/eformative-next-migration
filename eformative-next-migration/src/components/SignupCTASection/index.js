99f33fb752625c626b4543c75867b7a9729eccfc
NekoCarreraDesigns <denvermaas@gmail.com>
2024-02-13 13:02:49 -0700

    changed classnames for next compatibility

--- eformative-next-migration/src/components/SignupCTASection/index.js
+++ eformative-next-migration/src/components/SignupCTASection/index.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-// import "./SignupCTASection.css";
+import { useRouter } from "next/router";
+import styles from "./SignupCTASection.module.css";
 
 const SignupCTASection = () => {
-  let navigate = useNavigate();
+  let navigate = useRouter();
 
   const signUpRedirect = () => {
     let signupPath = `/signup`;
-    navigate(signupPath);
+    navigate.push(signupPath);
   };
 
   return (
-    <div className='signup-cta-section'>
-      <div className='container'>
-        <div className='two-columns-section'>
-          <div className='left-column'>
+    <div className={styles.signupSection}>
+      <div className={styles.container}>
+        <div className={styles.twoColumnsSection}>
+          <div className={styles.leftColumn}>
             <img
-              className='signup-cta-image'
+              className={styles.signupCtaImage}
               alt='Couple on e-Scooter'
               src='/images/emotoscooter-cut.png'
             />
           </div>
-          <div className='right-column'>
-            <h2 className='cta-header'>Join the Green Marketplace</h2>
-            <p className='cta-body'>
+          <div className={styles.rightColumn}>
+            <h2 className={styles.signupCtaHeader}>Join the Green Marketplace</h2>
+            <p className={styles.signupCtaBody}>
               Contribute to a sustainable future by buying and selling electric
               products with ease.
             </p>
-            <button className='signup-cta-button' onClick={signUpRedirect} aria-label="button to join the marketplace">
+            <button className={styles.signupCtaButton} onClick={signUpRedirect} aria-label="button to join the marketplace">
               Sign Up Now
             </button>
           </div>
