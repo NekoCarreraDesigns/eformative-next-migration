ba59ceeed6feacbe026a5c547c6fbd10ce12e2e2
NekoCarreraDesigns <denvermaas@gmail.com>
2024-02-19 11:15:50 -0700

    created app directory, and moved files into folders

---
+++ eformative-next-migration/src/app/About/about.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <>
+      <div className="home-hero">
+        <h1 className='about-page-header'>About Us</h1>
+        <p className='about-page-paragraph'>
+          Eformative site created by Quality Networks, graphics by Hager Creative, site concept by Scott Harrah.
+          <br />
+          The site was created as an alternative marketplace for electric
+          transportation alternatives to create a greener environment for all of
+          us to live in. It is our belief that we can make the world a better
+          place!
+
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ eformative-next-migration/src/app/Landing/landing.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./landing.module.css";
+import { useRouter } from 'next/router'
+import SignupCTASection from "../../components/SignupCTASection";
+import IconsSection from "../../components/Icons";
+
+export default function Landing () {
+  let navigate = useRouter();
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log("position:", position)
+      console.log("current location:", window.location.href)
+      navigate.push("/market");
+    });
+  };
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.homeHero}>
+        <img
+          className={styles.heroLogo}
+          alt='eformative-logo'
+          src='/images/eformative-logo-white.png'
+        />
+        <h2 className={styles.homePageHeader}>Have Fun, and help the planet!</h2>
+        <div className={styles.buttonContainer}>
+          <button className='clear-btn-green-border' onClick={getLocation}>
+            Find Your Market
+          </button>
+        </div>
+        <IconsSection />
+      </div>
+      <SignupCTASection />
+    </div>
+  );
+};
+
